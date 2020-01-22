ab25d560ba2e8f4ba466626b3e85f1690e8a3e91
lacoludo <email@lacoludo.com>
2020-01-22 05:21:11 +0100

    Transformer plugins to transform Markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,11 +7,12 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-remark`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`
+        name: `pages`,
+        path: `${__dirname}/src/pages`
       }
     },
     `gatsby-transformer-sharp`,
